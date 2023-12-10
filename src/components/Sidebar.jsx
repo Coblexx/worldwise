55568e88c6a2f2c50089ee096c482a21bf06d19f
radek <radek.glogowski3@gmail.com>
2023-12-10 17:01:45 +0100

    chore: create footer component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,17 @@
+import Logo from "./Logo.jsx";
+import AppNav from "./AppNav.jsx";
+import styles from "./Sidebar.module.css";
+import Footer from "./Footer.jsx";
+
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
 }
 
 export default Sidebar;
